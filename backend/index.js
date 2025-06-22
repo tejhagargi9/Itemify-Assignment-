1c22c3aa531ecca17eb1e858ff8e3f7424aa42d6
Tej MH <tejhagargigit@gmail.com>
2025-06-22 17:33:30 +0530

    refactored mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -9,18 +9,23 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ MongoDB connection error:", err)); 
 
-const addItems = require("./routes/addItems");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+
+const addItems = require("./routes/addItems"); 
 const getItems = require("./routes/getItems");
 
 app.use(addItems);
 app.use(getItems);
 
 app.get("/", (req, res) => { 
-  res.send("✅ Backend running with Cloudinary and MongoDB");
+  res.send("✅ Backend running with Cloudinary and MongoDB"); 
 });
 
 app.listen(PORT, () => {
